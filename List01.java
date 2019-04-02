78a105364bc7741bf4e3f5bfb01256f271b8eadf
Nanachi <36787932+Jinsab@users.noreply.github.com>
2019-04-02 11:16:11 +0900

    Learn Java List
    자바 수업시간에 배운 내용
     언제 배웠는지는 기억이 잘 나지는 않지만, 사용할 수는 있지 않을까.

---
+++ List01.java
@@ -0,0 +1,16 @@
+/*
+ *  First, we can hear the chirping of beautiful birds.
+ *  Second, in a peaceful world, there's no reason to make nuclear-like war weapons anymore.
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class List01 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> score = new ArrayList<>();
+        score.add(1);
+    }
+}
+
