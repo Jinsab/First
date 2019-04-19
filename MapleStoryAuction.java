0629eb552e05ffc2099207ad8d0722348022b857
Nanachi <36787932+Jinsab@users.noreply.github.com>
2019-04-19 16:06:42 +0900

    Create MapleStoryAuction
    메이플스토리 장터 시스템을 구현해보았다.

---
+++ MapleStoryAuction.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class MapleStoryAuction {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        EnterAuction Enter;
+
+        while (true) {
+
+        }
+    }
+
+    class itemName { // 아이템 이름
+
+    }
+
+    class theVolumeOfTrafficItem { // 아이템 갯수
+
+    }
+
+    class itemPrice { // 아이템 개당 가격
+
+    }
+
+    class itemAllPrice { // 아이템 전체 가격
+
+    }
+
+    class itemRemainingTime { // 아이템 남은 시간
+
+    }
+
+    class Purchaser { // 아이템 구매 시간
+
+    }
+
+    class Sellers { // 아이템 판매 시간
+
+    }
+
+    class ItemPurchase { // 아이템 구매
+
+    }
+
+    class ItemSales { // 아이템 판매
+
+    }
+
+    class ReceivingMeso { // 메소 수령
+
+    }
+
+    class EnterAuction { // 경매장 입장
+
+    }
+}
